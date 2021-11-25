8ae24d49dd9ba0df490791fd5903d94876202078
Luis Eduardo Bolivar Cardenas <theluisbolivar@Luiss-MacBook-Pro.local>
2021-11-25 15:21:57 -0600

    finish naive approach of segment tree

--- src/com/co/pa/Main.java
+++ src/com/co/pa/Main.java
@@ -5,14 +5,13 @@ public class Main {
     public static void main(String[] args) {
         int[] test = new int[] {1, 6, -23, 4, -5, 9, -19};
         SegmentTree segmentTree = new SegmentTree(test);
+        int result = segmentTree.sum(0, 2);
+        boolean resultBoolean = segmentTree.update(0, 100);
     }
 }
 
-
-
 class SegmentTree {
     int[] arr;
-
     int[][] helper;
 
     SegmentTree(int[] array) {
@@ -20,15 +19,32 @@ class SegmentTree {
         fillHelper();
     }
 
-    int sum(int a, int b){
-        return 0;
+    int sum(int a, int b) {
+        return helper[a][b];
     }
 
-    boolean update(int i, int value){
+    boolean update(int i, int value) {
+        arr[i] = value;
+        fillHelper();
         return true;
     }
 
     void fillHelper(){
+        int size = arr.length;
+        helper = new int[size][size];
 
+        for(int i = 0; i < size; i++) {
+            int arri = arr[i];
+            for (int j = i; j < size; j++) {
+                int arrj = arr[j];
+
+                if(i == j) {
+                    helper[i][j] = arri;
+                } else {
+                    helper[i][j] = arri + arrj;
+                    arri = helper[i][j];
+                }
+            }
+        }
     }
 }
