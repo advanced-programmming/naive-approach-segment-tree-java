07478d127186eeb9b531247a2c1b83d4ca951f60
Luis Eduardo Bolivar Cardenas <theluisbolivar@Luiss-MacBook-Pro.local>
2021-11-25 14:58:25 -0600

    add segment tree structure

--- src/com/co/pa/Main.java
+++ src/com/co/pa/Main.java
@@ -3,6 +3,32 @@ package com.co.pa;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] test = new int[] {1, 6, -23, 4, -5, 9, -19};
+        SegmentTree segmentTree = new SegmentTree(test);
+    }
+}
+
+
+
+class SegmentTree {
+    int[] arr;
+
+    int[][] helper;
+
+    SegmentTree(int[] array) {
+        arr = array;
+        fillHelper();
+    }
+
+    int sum(int a, int b){
+        return 0;
+    }
+
+    boolean update(int i, int value){
+        return true;
+    }
+
+    void fillHelper(){
+
     }
 }
